0516ac8307a2c14279a64e7096fb25fc71218d95
Kenny Hearn <wkhearn@gmail.com>
2017-05-04 04:49:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < collection.length
+      new_collection << yield(collection[i])
+      i += 1
+    end
+    new_collection
+  end
+end
